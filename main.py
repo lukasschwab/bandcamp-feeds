b2cc96a8e29d4b4bb344c5e5b8373efc6fadf915
Lukas Schwab <lukas.schwab@gmail.com>
2025-09-29 11:39:46 -0700

    Nit: snake-case

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup as bs
 BASE_URL_FORMAT = "https://{category}.bandcamp.com/music"
 MAX_ITEMS = 20
 
-def toItem(page_item, request_url):
+def to_item(page_item, request_url):
     relative_path = page_item.a["href"]
     return jf.Item(
         id=relative_path,
@@ -18,7 +18,7 @@ def toItem(page_item, request_url):
 def page_to_items(page):
     soup = bs(page.text, 'html.parser')
     page_items = soup.findAll("li", class_="music-grid-item")
-    return [toItem(page_item, page.url) for page_item in page_items[:MAX_ITEMS]]
+    return [to_item(page_item, page.url) for page_item in page_items[:MAX_ITEMS]]
 
 wrapper = jfw.JSONFeedWrapper("Bandcamp", BASE_URL_FORMAT, page_to_items, MAX_ITEMS)
 
